a1c75a7146cfdabff3dde037ee18611929cb6322
Jabe-97 <jabraham97@outlook.com>
2021-04-10 20:24:47 -0400

    Initial Commit

---
+++ src/test/java/com/crimeinsc/crimeinscrest/CrimeInScRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crimeinsc.crimeinscrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrimeInScRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
